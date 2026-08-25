297bc0fd40298f84356c290c85246f788f77eaf4
Niraj Kumar Dewangan <niraj.dewangan1608@gmail.com>
2026-08-25 23:44:30 +0530

    Task 2 - 01 dev profile variable

---
+++ Task2/01-developer-profile-variables/dev.py
@@ -0,0 +1,34 @@
+name = "Niraj"
+age = 36
+city = "Bangalore"
+company = "Euron"
+current_learning_topic = "Python"
+years_of_experience = 10
+like_python = True
+years_of_experience_in_python = 0
+expected_salary = 15000000.00
+
+
+#Printing the developer profile
+print("Developer Profile:")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Company:", company)
+print("Current Learning Topic:", current_learning_topic)
+print("Years of Experience:", years_of_experience)
+print("Like Python:", like_python)
+print("Years of Experience in Python:", years_of_experience_in_python)
+print("Expected Salary:", expected_salary)
+
+#Printing the type of each variable
+print("\nType of each variable:")
+print("Type of Name:", type(name))
+print("Type of Age:", type(age))
+print("Type of City:", type(city))
+print("Type of Company:", type(company))
+print("Type of Current Learning Topic:", type(current_learning_topic))
+print("Type of Years of Experience:", type(years_of_experience))
+print("Type of Like Python:", type(like_python))
+print("Type of Years of Experience in Python:", type(years_of_experience_in_python))
+print("Type of Expected Salary:", type(expected_salary))
